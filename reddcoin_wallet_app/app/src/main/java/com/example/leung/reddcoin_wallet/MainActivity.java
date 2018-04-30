d29d27c6004760c27b64da12c5a4bfc4d439da8c
Leung Cheng <monewckl@hotmail.com.hk>
2018-04-30 21:50:41 +0800

    some interface add

--- reddcoin_wallet_app/app/src/main/java/com/example/leung/reddcoin_wallet/MainActivity.java
+++ reddcoin_wallet_app/app/src/main/java/com/example/leung/reddcoin_wallet/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.leung.reddcoin_wallet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        android.view.MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mybar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_friend:
+                android.content.Intent intent = new android.content.Intent(this, FriendActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_setting:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goSend(View view){
+        android.content.Intent intent = new android.content.Intent(this, SendActivity.class);
+        startActivity(intent);
+    }
+
+    public void goRequest(View view){
+        android.content.Intent intent = new android.content.Intent(this, RequestActivity.class);
+        startActivity(intent);
     }
 }
