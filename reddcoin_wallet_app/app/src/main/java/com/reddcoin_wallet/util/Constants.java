2bfd2bb90f74cb3e6f181cbfd7bb45ee20b41040
Unknown <glauserr@ethz.ch>
2018-05-04 11:47:31 +0800

    Further utils for server connection added

--- reddcoin_wallet_app/app/src/main/java/com/reddcoin_wallet/util/Constants.java
+++ reddcoin_wallet_app/app/src/main/java/com/reddcoin_wallet/util/Constants.java
@@ -78,12 +78,11 @@ public class Constants {
     // TODO move to resource files
     public static final List<CoinAddress> DEFAULT_COINS_SERVERS = ImmutableList.of(
             new CoinAddress(ReddcoinMain.get(),
-                    new ServerAddress("rdd-cce-1.reddcoin.net", 5014),
-                    new ServerAddress("rdd-cce-2.reddcoin.net", 5014),
-                    new ServerAddress("reddwallet.org", 50001))
+                    new ServerAddress("10.0.2.2", 8081),
+                    new ServerAddress("10.0.2.2", 50001),
     );
 
-    //public static final Integer COIN_ICON = R.drawable.reddcoin;
+    public static final Integer COIN_ICON = R.drawable.green_circle;
     public static final String COINS_BLOCK_EXPLORER = "http://live.reddcoin.com/tx/%s";
 
     public static final CoinType DEFAULT_COIN = ReddcoinMain.get();
