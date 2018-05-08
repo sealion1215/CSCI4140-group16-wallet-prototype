d8641c1c5a9b5f8521d817a17ee92f027b2c7b0f
Leung Cheng <monewckl@hotmail.com.hk>
2018-05-08 19:16:02 +0800

    incomplete code

--- reddcoin_wallet_app/coinomi-android-master/wallet/src/main/java/com/reddcoin/wallet/ui/FriendsActivity.java
+++ reddcoin_wallet_app/coinomi-android-master/wallet/src/main/java/com/reddcoin/wallet/ui/FriendsActivity.java
@@ -1,8 +1,11 @@
 package com.reddcoin.wallet.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,6 +32,35 @@ public class FriendsActivity extends BaseWalletActivity{
 //        getSupportActionBar().setDisplayShowHomeEnabled(false);
     }
 
+    public class Friend
+    {
+        public final String name, address;
+
+        public Friend(String iName, String iAddress)
+        {
+            name   = iName;
+            address = iAddress;
+        }
+    }
+
+    private boolean saveArray(String[] array, String arrayName, Context mContext) {
+        SharedPreferences prefs = mContext.getSharedPreferences(getString(R.string.friend_list_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(arrayName +"_size", array.length);
+        for(int i=0;i<array.length;i++)
+            editor.putString(arrayName + "_" + i, array[i]);
+        return editor.commit();
+    }
+
+    private String[] loadArray(String arrayName, Context mContext) {
+        SharedPreferences prefs = mContext.getSharedPreferences(getString(R.string.friend_list_key), Context.MODE_PRIVATE);
+        int size = prefs.getInt(arrayName + "_size", 0);
+        String array[] = new String[size];
+        for(int i=0;i<size;i++)
+            array[i] = prefs.getString(arrayName + "_" + i, null);
+        return array;
+    }
+
     public void addFriend(View view){
         EditText nameText = findViewById(R.id.inputName);
         EditText addressText = findViewById(R.id.inputAddress);
@@ -36,7 +68,20 @@ public class FriendsActivity extends BaseWalletActivity{
         String friendAddress = addressText.getText().toString();
         if (! (friendName.isEmpty() || friendAddress.isEmpty()) ){
             if(validate(friendAddress)){
+                //hide keyboard
+                View temp = this.getCurrentFocus();
+                if (temp != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
 
+                //clear input box
+                nameText.getText().clear();
+                addressText.getText().clear();
+
+
+
+                Toast.makeText(this, "Added!", Toast.LENGTH_SHORT).show();
             }else{
                 Toast.makeText(this, "Invalid Address.", Toast.LENGTH_SHORT).show();
             }
